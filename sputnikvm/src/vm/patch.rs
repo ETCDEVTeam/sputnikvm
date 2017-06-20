1a299887d005e42f32b82d12b2bcddcd4ac1ceb7
Wei Tang <hi@that.world>
2017-06-21 00:06:27 +0800

    release v0.2: remove unstable patches

--- sputnikvm/src/vm/patch.rs
+++ sputnikvm/src/vm/patch.rs
@@ -24,19 +24,6 @@ pub static FRONTIER_PATCH: Patch = Patch {
     force_code_deposit: true,
 };
 
-pub static HOMESTEAD_PATCH: Patch = Patch {
-    callstack_limit: 1024,
-    gas_extcode: 20,
-    gas_balance: 20,
-    gas_sload: 50,
-    gas_suicide: 0,
-    gas_suicide_new_account: 0,
-    gas_call: 40,
-    gas_expbyte: 10,
-    gas_transaction_create: 32000,
-    force_code_deposit: false,
-};
-
 pub static VMTEST_PATCH: Patch = Patch {
     callstack_limit: 2,
     gas_extcode: 20,
@@ -48,30 +35,4 @@ pub static VMTEST_PATCH: Patch = Patch {
     gas_expbyte: 10,
     gas_transaction_create: 0,
     force_code_deposit: true,
-};
-
-pub static EIP150_PATCH: Patch = Patch {
-    callstack_limit: 1024,
-    gas_extcode: 700,
-    gas_balance: 400,
-    gas_sload: 200,
-    gas_suicide: 5000,
-    gas_suicide_new_account: 25000,
-    gas_call: 700,
-    gas_expbyte: 10,
-    gas_transaction_create: 32000,
-    force_code_deposit: false,
-};
-
-pub static EIP160_PATCH: Patch = Patch {
-    callstack_limit: 1024,
-    gas_extcode: 700,
-    gas_balance: 400,
-    gas_sload: 200,
-    gas_suicide: 5000,
-    gas_suicide_new_account: 25000,
-    gas_call: 700,
-    gas_expbyte: 50,
-    gas_transaction_create: 32000,
-    force_code_deposit: false,
 };
