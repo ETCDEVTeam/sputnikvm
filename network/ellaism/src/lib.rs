rust-ethereum/evm

Specify patches for supported networks
Summary:
This moves Classic patches to a separate crate, and add additional patches for
Foundation, Ellaism, Musicoin, Expanse, Ubiq.

Reviewers: sorpaas

Reviewed By: sorpaas

Subscribers: jenkins

Differential Revision: https://source.that.world/D46

network/ellaism/src/lib.rs
@@ -1,0 +1,58 @@
+extern crate bigint;
+extern crate evm;
+
+use std::marker::PhantomData;
+use bigint::{Gas, U256, H160, Address};
+use evm::{Precompiled, AccountPatch, Patch,
+          ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+
+pub static ELLA_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 4] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+];
+
+/// Mainnet account patch
+pub struct MainnetAccountPatch;
+impl AccountPatch for MainnetAccountPatch {
+    fn initial_nonce() -> U256 { U256::zero() }
+    fn initial_create_nonce() -> U256 { Self::initial_nonce() }
+    fn empty_considered_exists() -> bool { true }
+}
+
+/// EIP160 patch.
+pub struct EIP160Patch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetEIP160Patch = EIP160Patch<MainnetAccountPatch>;
+impl<A: AccountPatch> Patch for EIP160Patch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(700usize) }
+    fn gas_balance() -> Gas { Gas::from(400usize) }
+    fn gas_sload() -> Gas { Gas::from(200usize) }
+    fn gas_suicide() -> Gas { Gas::from(5000usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(25000usize) }
+    fn gas_call() -> Gas { Gas::from(700usize) }
+    fn gas_expbyte() -> Gas { Gas::from(50usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { false }
+    fn call_create_l64_after_gas() -> bool { true }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &ELLA_PRECOMPILEDS }
+}

network/musicoin/src/lib.rs
@@ -1,0 +1,86 @@
+extern crate bigint;
+extern crate evm;
+
+use std::marker::PhantomData;
+use bigint::{Gas, U256, H160, Address};
+use evm::{Precompiled, AccountPatch, Patch,
+          ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+
+/// Mainnet account patch
+pub struct MainnetAccountPatch;
+impl AccountPatch for MainnetAccountPatch {
+    fn initial_nonce() -> U256 { U256::zero() }
+    fn initial_create_nonce() -> U256 { Self::initial_nonce() }
+    fn empty_considered_exists() -> bool { true }
+}
+
+pub static MUSIC_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 4] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+];
+
+/// Frontier patch.
+pub struct FrontierPatch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetFrontierPatch = FrontierPatch<MainnetAccountPatch>;
+impl<A: AccountPatch> Patch for FrontierPatch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(20usize) }
+    fn gas_balance() -> Gas { Gas::from(20usize) }
+    fn gas_sload() -> Gas { Gas::from(50usize) }
+    fn gas_suicide() -> Gas { Gas::from(0usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(0usize) }
+    fn gas_call() -> Gas { Gas::from(40usize) }
+    fn gas_expbyte() -> Gas { Gas::from(10usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(0usize) }
+    fn force_code_deposit() -> bool { true }
+    fn has_delegate_call() -> bool { false }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { true }
+    fn call_create_l64_after_gas() -> bool { false }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &MUSIC_PRECOMPILEDS }
+}
+
+/// Homestead patch.
+pub struct HomesteadPatch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetHomesteadPatch = HomesteadPatch<MainnetAccountPatch>;
+impl<A: AccountPatch> Patch for HomesteadPatch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(20usize) }
+    fn gas_balance() -> Gas { Gas::from(20usize) }
+    fn gas_sload() -> Gas { Gas::from(50usize) }
+    fn gas_suicide() -> Gas { Gas::from(0usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(0usize) }
+    fn gas_call() -> Gas { Gas::from(40usize) }
+    fn gas_expbyte() -> Gas { Gas::from(10usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { true }
+    fn call_create_l64_after_gas() -> bool { false }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &MUSIC_PRECOMPILEDS }
+}

network/ubiq/src/lib.rs
@@ -1,0 +1,55 @@
+extern crate bigint;
+extern crate evm;
+
+use bigint::{Gas, U256, H160, Address};
+use evm::{Precompiled, AccountPatch, Patch,
+          ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+
+pub static FRONTIER_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 4] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+];
+
+pub struct StateClearingAccountPatch;
+impl AccountPatch for StateClearingAccountPatch {
+    fn initial_nonce() -> U256 { U256::zero() }
+    fn initial_create_nonce() -> U256 { Self::initial_nonce() + U256::from(1) }
+    fn empty_considered_exists() -> bool { false }
+}
+
+/// Spurious Dragon patch.
+pub struct SpuriousDragonPatch;
+impl Patch for SpuriousDragonPatch {
+    type Account = StateClearingAccountPatch;
+
+    fn code_deposit_limit() -> Option<usize> { Some(0x6000) }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(700usize) }
+    fn gas_balance() -> Gas { Gas::from(400usize) }
+    fn gas_sload() -> Gas { Gas::from(200usize) }
+    fn gas_suicide() -> Gas { Gas::from(5000usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(25000usize) }
+    fn gas_call() -> Gas { Gas::from(700usize) }
+    fn gas_expbyte() -> Gas { Gas::from(50usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { false }
+    fn call_create_l64_after_gas() -> bool { true }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &FRONTIER_PRECOMPILEDS }
+}

regtests/src/bin/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate clap;
 extern crate evm;
+extern crate evm_network_classic;
 extern crate serde_json;
 extern crate gethrpc;
 extern crate block;
@@ -20,8 +21,9 @@ use block::TransactionAction;
 use bigint::{Gas, Address, U256, M256, H256};
 use hexutil::*;
 use evm::{HeaderParams, Context, SeqTransactionVM, ValidTransaction, VM, Log, Patch,
-                AccountCommitment, AccountChange, MainnetFrontierPatch, MainnetHomesteadPatch,
-                MainnetEIP150Patch, MainnetEIP160Patch};
+          AccountCommitment, AccountChange};
+use evm_network_classic::{MainnetFrontierPatch, MainnetHomesteadPatch,
+                          MainnetEIP150Patch, MainnetEIP160Patch};
 use evm::errors::RequireError;
 use gethrpc::{GethRPCClient, NormalGethRPCClient, RecordGethRPCClient, CachedGethRPCClient, RPCCall, RPCBlock, RPCTransaction, RPCLog};
 

ETCDEVTeam/sputnikvm

network/ellaism/src/lib.rs
@@ -1,0 +1,58 @@
+extern crate bigint;
+extern crate sputnikvm;
+
+use std::marker::PhantomData;
+use bigint::{Gas, U256, H160, Address};
+use sputnikvm::{Precompiled, AccountPatch, Patch,
+                ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+
+pub static ELLA_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 4] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+];
+
+/// Mainnet account patch
+pub struct MainnetAccountPatch;
+impl AccountPatch for MainnetAccountPatch {
+    fn initial_nonce() -> U256 { U256::zero() }
+    fn initial_create_nonce() -> U256 { Self::initial_nonce() }
+    fn empty_considered_exists() -> bool { true }
+}
+
+/// EIP160 patch.
+pub struct EIP160Patch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetEIP160Patch = EIP160Patch<MainnetAccountPatch>;
+impl<A: AccountPatch> Patch for EIP160Patch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(700usize) }
+    fn gas_balance() -> Gas { Gas::from(400usize) }
+    fn gas_sload() -> Gas { Gas::from(200usize) }
+    fn gas_suicide() -> Gas { Gas::from(5000usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(25000usize) }
+    fn gas_call() -> Gas { Gas::from(700usize) }
+    fn gas_expbyte() -> Gas { Gas::from(50usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { false }
+    fn call_create_l64_after_gas() -> bool { true }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &ELLA_PRECOMPILEDS }
+}

network/musicoin/src/lib.rs
@@ -1,0 +1,86 @@
+extern crate bigint;
+extern crate sputnikvm;
+
+use std::marker::PhantomData;
+use bigint::{Gas, U256, H160, Address};
+use sputnikvm::{Precompiled, AccountPatch, Patch,
+                ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+
+/// Mainnet account patch
+pub struct MainnetAccountPatch;
+impl AccountPatch for MainnetAccountPatch {
+    fn initial_nonce() -> U256 { U256::zero() }
+    fn initial_create_nonce() -> U256 { Self::initial_nonce() }
+    fn empty_considered_exists() -> bool { true }
+}
+
+pub static MUSIC_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 4] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+];
+
+/// Frontier patch.
+pub struct FrontierPatch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetFrontierPatch = FrontierPatch<MainnetAccountPatch>;
+impl<A: AccountPatch> Patch for FrontierPatch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(20usize) }
+    fn gas_balance() -> Gas { Gas::from(20usize) }
+    fn gas_sload() -> Gas { Gas::from(50usize) }
+    fn gas_suicide() -> Gas { Gas::from(0usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(0usize) }
+    fn gas_call() -> Gas { Gas::from(40usize) }
+    fn gas_expbyte() -> Gas { Gas::from(10usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(0usize) }
+    fn force_code_deposit() -> bool { true }
+    fn has_delegate_call() -> bool { false }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { true }
+    fn call_create_l64_after_gas() -> bool { false }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &MUSIC_PRECOMPILEDS }
+}
+
+/// Homestead patch.
+pub struct HomesteadPatch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetHomesteadPatch = HomesteadPatch<MainnetAccountPatch>;
+impl<A: AccountPatch> Patch for HomesteadPatch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(20usize) }
+    fn gas_balance() -> Gas { Gas::from(20usize) }
+    fn gas_sload() -> Gas { Gas::from(50usize) }
+    fn gas_suicide() -> Gas { Gas::from(0usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(0usize) }
+    fn gas_call() -> Gas { Gas::from(40usize) }
+    fn gas_expbyte() -> Gas { Gas::from(10usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { true }
+    fn call_create_l64_after_gas() -> bool { false }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &MUSIC_PRECOMPILEDS }
+}

network/ubiq/src/lib.rs
@@ -1,0 +1,55 @@
+extern crate bigint;
+extern crate sputnikvm;
+
+use bigint::{Gas, U256, H160, Address};
+use sputnikvm::{Precompiled, AccountPatch, Patch,
+                ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+
+pub static FRONTIER_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 4] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+];
+
+pub struct StateClearingAccountPatch;
+impl AccountPatch for StateClearingAccountPatch {
+    fn initial_nonce() -> U256 { U256::zero() }
+    fn initial_create_nonce() -> U256 { Self::initial_nonce() + U256::from(1) }
+    fn empty_considered_exists() -> bool { false }
+}
+
+/// Spurious Dragon patch.
+pub struct SpuriousDragonPatch;
+impl Patch for SpuriousDragonPatch {
+    type Account = StateClearingAccountPatch;
+
+    fn code_deposit_limit() -> Option<usize> { Some(0x6000) }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(700usize) }
+    fn gas_balance() -> Gas { Gas::from(400usize) }
+    fn gas_sload() -> Gas { Gas::from(200usize) }
+    fn gas_suicide() -> Gas { Gas::from(5000usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(25000usize) }
+    fn gas_call() -> Gas { Gas::from(700usize) }
+    fn gas_expbyte() -> Gas { Gas::from(50usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { false }
+    fn has_revert() -> bool { false }
+    fn has_return_data() -> bool { false }
+    fn err_on_call_with_more_gas() -> bool { false }
+    fn call_create_l64_after_gas() -> bool { true }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &FRONTIER_PRECOMPILEDS }
+}

regtests/src/bin/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate clap;
 extern crate sputnikvm;
+extern crate sputnikvm_network_classic;
 extern crate serde_json;
 extern crate gethrpc;
 extern crate block;
@@ -20,8 +21,9 @@ use block::TransactionAction;
 use bigint::{Gas, Address, U256, M256, H256};
 use hexutil::*;
 use sputnikvm::{HeaderParams, Context, SeqTransactionVM, ValidTransaction, VM, Log, Patch,
-                AccountCommitment, AccountChange, MainnetFrontierPatch, MainnetHomesteadPatch,
-                MainnetEIP150Patch, MainnetEIP160Patch};
+                AccountCommitment, AccountChange};
+use sputnikvm_network_classic::{MainnetFrontierPatch, MainnetHomesteadPatch,
+                                MainnetEIP150Patch, MainnetEIP160Patch};
 use sputnikvm::errors::RequireError;
 use gethrpc::{GethRPCClient, NormalGethRPCClient, RecordGethRPCClient, CachedGethRPCClient, RPCCall, RPCBlock, RPCTransaction, RPCLog};
 