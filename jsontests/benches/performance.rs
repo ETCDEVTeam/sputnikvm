7878fd4b348fca8c691e7b48e6c868be7f62a131
Mike Lubinets <lubinetsm@yandex.ru>
2018-08-24 00:34:24 +0300

    proc-macro tests generation  - tests generation macro  - benchmarking support prototype

--- jsontests/benches/performance.rs
+++ jsontests/benches/performance.rs
@@ -1,58 +1,13 @@
 #![feature(test)]
+#![allow(non_snake_case)]
 
-extern crate test;
-extern crate jsontests;
-extern crate serde_json;
 #[macro_use]
-extern crate lazy_static;
+extern crate jsontests_derive;
+extern crate jsontests;
+extern crate test;
 
-use test::Bencher;
-use serde_json::Value;
-use jsontests::test_transaction;
-
-lazy_static! {
-    static ref TESTS: Value =
-        serde_json::from_str(include_str!("../res/files/vmPerformanceTest.json")).unwrap();
-}
-
-#[bench]
-fn ackermann31(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(test_transaction("ackermann31", &TESTS["ackermann31"], false), Ok(true))
-    });
-}
-
-#[bench]
-fn ackermann32(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(test_transaction("ackermann32", &TESTS["ackermann32"], false), Ok(true))
-    });
-}
-
-#[bench]
-fn ackermann33(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(test_transaction("ackermann33", &TESTS["ackermann33"], false), Ok(true))
-    });
-}
-
-#[bench]
-fn fibonacci10(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(test_transaction("fibonacci10", &TESTS["fibonacci10"], false), Ok(true))
-    });
-}
-
-#[bench]
-fn fibonacci16(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(test_transaction("fibonacci16", &TESTS["fibonacci16"], false), Ok(true))
-    })
-}
-
-#[bench]
-fn many_functions100(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(test_transaction("manyFunctions100", &TESTS["manyFunctions100"], false), Ok(true))
-    });
-}
+#[derive(JsonTests)]
+#[directory = "jsontests/res/files/vmPerformance"]
+#[test_with = "jsontests::util::run_test"]
+#[bench_with = "jsontests::util::run_bench"]
+struct Performance;
