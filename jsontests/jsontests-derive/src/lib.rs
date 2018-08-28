de053c4c6e190e1ec017dbc7a99d607ac258745e
Mike Lubinets <lubinetsm@yandex.ru>
2018-08-28 18:09:23 +0300

    generator refactoring

--- jsontests/jsontests-derive/src/lib.rs
+++ jsontests/jsontests-derive/src/lib.rs
@@ -10,44 +10,24 @@ extern crate failure;
 extern crate itertools;
 extern crate rayon;
 
-use rayon::prelude::*;
+mod attr;
+mod tests;
+mod util;
 
-use std::str::FromStr;
-use std::collections::HashSet;
-use std::ascii::AsciiExt;
-use std::path::{Path, PathBuf};
-use std::fs::{self, File, DirEntry};
+use attr::extract_attrs;
+use tests::read_tests_from_dir;
+use util::*;
+
 use failure::Error;
 use itertools::Itertools;
 
-use syn::Token::{
-    Literal  as TLiteral,
-    FatArrow as TFatArrow,
-    Ident    as TIdent,
-    ModSep   as TModSep,
-    Pound    as TPound
-};
-use syn::TokenTree::{
-    self,
-    Token     as TTToken,
-    Delimited as TTDelimited
-};
-use syn::Lit::Str;
 use syn::Ident;
-use syn::MetaItem;
-use syn::Delimited;
-use syn::DelimToken::Bracket;
-use quote::Tokens;
 use proc_macro::TokenStream;
-use json::Value;
-use std::time::Instant;
 
 #[proc_macro_derive(JsonTests, attributes(directory, test_with, bench_with, skip, should_panic))]
 pub fn json_tests(input: TokenStream) -> TokenStream {
     // Construct a string representation of the type definition
     let s = input.to_string();
-
-
 
     // Parse the string representation
     let ast = syn::parse_derive_input(&s).unwrap();
@@ -58,7 +38,6 @@ pub fn json_tests(input: TokenStream) -> TokenStream {
         Err(err) => panic!("{}", err)
     };
 
-
     // Return the generated impl
     let parsed = gen.parse().unwrap();
 
@@ -66,7 +45,6 @@ pub fn json_tests(input: TokenStream) -> TokenStream {
 }
 
 fn impl_json_tests(ast: &syn::DeriveInput) -> Result<quote::Tokens, Error> {
-    let name = &ast.ident;
     let config = extract_attrs(&ast)?;
     let tests = read_tests_from_dir(&config.directory)?;
     let mut tokens = quote::Tokens::new();
@@ -74,17 +52,11 @@ fn impl_json_tests(ast: &syn::DeriveInput) -> Result<quote::Tokens, Error> {
     // split tests into groups by filepath
     let tests = tests.into_iter().group_by(|test| test.path.clone());
 
-    // create identifiers
-    let test_func_name = config.test_func.rsplit(":").next().unwrap();
-    let test_func_path = Ident::from(config.test_func.as_ref());
-    let test_func_name = Ident::from(test_func_name);
-
     open_directory_module(&config, &mut tokens);
 
     for (filepath, tests) in &tests {
-        let tests = tests.collect::<Vec<_>>();
-
         // If tests count in this file is 1, we don't need submodule
+        let tests = tests.collect::<Vec<_>>();
         let need_file_submodule = tests.len() > 1;
 
         if need_file_submodule {
@@ -93,28 +65,11 @@ fn impl_json_tests(ast: &syn::DeriveInput) -> Result<quote::Tokens, Error> {
 
         // Generate test function
         for test in tests {
+            let ref test_func_path = config.test_with.path;
+            let ref test_func_name = config.test_with.name;
             let name = sanitize_ident(&test.name);
             let name_ident = Ident::from(name.as_ref());
             let data = json::to_string(&test.data)?;
-
-            // generate optional benchmark body
-            if let Some(ref bench_func_path) = config.bench_func {
-                // create identifiers
-                let bench_func_name = bench_func_path.rsplit(":").next().unwrap();
-                let bench_func_path = Ident::from(bench_func_path.as_ref());
-                let bench_func_name = Ident::from(bench_func_name);
-                let name = format!("bench_{}", name);
-                let name_ident = Ident::from(name.as_ref());
-
-                tokens.append(quote! {
-                    #[bench]
-                    fn #name_ident(b: &mut test::Bencher) {
-                        use #bench_func_path;
-                        let data = #data;
-                        #bench_func_name(b, #name, data);
-                    }
-                })
-            }
 
             // generate test attrs
             tokens.append(quote!{#[test]});
@@ -130,6 +85,23 @@ fn impl_json_tests(ast: &syn::DeriveInput) -> Result<quote::Tokens, Error> {
                     #test_func_name(#name, data);
                 }
             });
+
+            // generate optional benchmark body
+            if let Some(ref bench) = config.bench_with {
+                let ref bench_func_path = bench.path;
+                let ref bench_func_name = bench.name;
+                let name = format!("bench_{}", name);
+                let name_ident = Ident::from(name.as_ref());
+
+                tokens.append(quote! {
+                    #[bench]
+                    fn #name_ident(b: &mut test::Bencher) {
+                        use #bench_func_path;
+                        let data = #data;
+                        #bench_func_name(b, #name, data);
+                    }
+                })
+            }
         }
 
         if need_file_submodule {
@@ -144,156 +116,5 @@ fn impl_json_tests(ast: &syn::DeriveInput) -> Result<quote::Tokens, Error> {
     Ok(tokens)
 }
 
-fn open_directory_module(config: &Config, tokens: &mut quote::Tokens) {
-    // get the leaf directory name
-    let dirname = config.directory.rsplit("/").next().unwrap();
 
-    // create identifier
-    let dirname = sanitize_ident(dirname);
-    let dirname = Ident::from(dirname);
 
-    open_module(dirname, tokens);
-}
-
-fn open_file_module(filepath: &str, tokens: &mut quote::Tokens) {
-    // get file name without extension
-    let filename = filepath.rsplit("/").next().unwrap()
-                           .split(".").next().unwrap();
-    // create identifier
-    let filename = sanitize_ident(filename);
-    let filename = Ident::from(filename);
-
-    open_module(filename, tokens);
-}
-
-fn open_module(module_name: Ident, tokens: &mut quote::Tokens) {
-    // append module opening tokens
-    tokens.append(quote! {
-        mod #module_name
-    });
-    tokens.append("{");
-}
-
-fn close_brace(tokens: &mut quote::Tokens) {
-    tokens.append("}")
-}
-
-fn sanitize_ident(ident: &str) -> String {
-    // replace empty ident
-    let ident = if ident.is_empty() {
-        String::from("unnamed")
-    } else { ident.to_string() };
-
-    // prepend alphabetic character if token starts with non-alphabetic character
-    let ident = if ident.chars().nth(0).map(|c| !c.is_alphabetic()).unwrap_or(true) {
-        format!("x_{}", ident)
-    } else { ident };
-
-    // replace special characters with _
-    let ident = replace_chars(&ident, "!@#$%^&*-+=/<>;\'\"()`~", "_");
-
-    ident
-}
-
-fn replace_chars(s: &str, from: &str, to: &str) -> String {
-    let mut initial = s.to_owned();
-    for c in from.chars() {
-        initial = initial.replace(c, to);
-    }
-    initial
-}
-
-fn extract_attrs(ast: &syn::DeriveInput) -> Result<Config, Error> {
-    const ERROR_MSG: &str = "expected 2 attributes and 1 optional\n\n\
-                #[derive(JsonTests)]\n\
-                #[directory = \"../tests/testset\"]\n\
-                #[test_with = \"test::test_function\"]\n\
-                #[bench_wuth = \"test::bench_function\"] (Optional)\n\
-                struct TestSet;";
-
-    if ast.attrs.len() < 2 || ast.attrs.len() > 3 {
-        bail!(ERROR_MSG);
-    }
-
-    let config = ast.attrs.iter().fold(Config::default(), |config, attr| {
-        match attr.value {
-            MetaItem::NameValue(ref ident, Str(ref value, _)) => {
-                match ident.as_ref() {
-                    "directory" => Config { directory: value.clone(), ..config },
-                    "test_with" => Config { test_func: value.clone(), ..config },
-                    "bench_with" => Config { bench_func: Some(value.clone()), ..config },
-                    _ => panic!("{}", ERROR_MSG),
-                }
-            },
-            MetaItem::Word(ref ident) => {
-                match ident.as_ref() {
-                    "skip" => Config { skip: true, ..config },
-                    "should_panic" => Config { should_panic: true, ..config },
-                    _ => panic!("{}", ERROR_MSG),
-                }
-            }
-            _ => panic!("{}", ERROR_MSG)
-        }
-    });
-
-    // Check for incompatible options
-    if config.should_panic && config.bench_func.is_some() {
-        panic!("#[should_panic] is incompatible with benchmark tests");
-    }
-
-    Ok(config)
-}
-
-#[derive(Default)]
-struct Config {
-    directory: String,
-    test_func: String,
-    bench_func: Option<String>,
-    skip: bool,
-    should_panic: bool,
-}
-
-fn read_tests_from_dir<P: AsRef<Path>>(dir_path: P) -> Result<impl Iterator<Item=Test>, Error> {
-    let dir = fs::read_dir(dir_path)?;
-
-    let iter = dir.into_iter()
-        .flat_map(|file|{
-            match file {
-                Ok(file) => tests_iterator_from_direntry(file).unwrap(),
-                Err(err) => panic!("failed to read dir: {}", err)
-            }
-        });
-
-    Ok(iter)
-}
-
-fn tests_iterator_from_direntry(file: DirEntry) -> Result<impl Iterator<Item=Test>, Error> {
-    let path = file.path().to_owned();
-    let file = File::open(&path)?;
-    let mut tests: Value = json::from_reader(file)?;
-
-    // Move out the root object
-    let tests = match tests {
-        Value::Object(tests) => tests,
-        _ => panic!("expected a json object at the root of test file")
-    };
-
-    let iter = tests.into_iter().map(move |(name, data)| Test {
-        path: path.to_str().unwrap().to_owned(),
-        name,
-        data
-    });
-
-    Ok(iter)
-}
-
-struct TestIterator {
-    dir: DirEntry,
-
-}
-
-struct Test {
-    path: String,
-    name: String,
-    data: Value
-}
