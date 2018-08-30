b51a65ad8a96673bf46975d4dcef55c8f711bfe3
Mike Lubinets <lubinetsm@yandex.ru>
2018-08-30 15:40:45 +0300

    switch to submodule for eth geth test suite

--- jsontests/tests/vmtests.rs
+++ jsontests/tests/vmtests.rs
@@ -10,67 +10,67 @@ extern crate jsontests;
 extern crate test;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmArithmeticTest"]
+#[directory = "jsontests/res/files/eth/VMTests/vmArithmeticTest"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct Arithmetic;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmBitwiseLogicOperation"]
+#[directory = "jsontests/res/files/eth/VMTests/vmBitwiseLogicOperation"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct BitwiseLogicOperation;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmBlockInfoTest"]
+#[directory = "jsontests/res/files/eth/VMTests/vmBlockInfoTest"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct BlockInfo;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmEnvironmentalInfo"]
+#[directory = "jsontests/res/files/eth/VMTests/vmEnvironmentalInfo"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct VmInverontemtalInfo;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmIOandFlowOperations"]
+#[directory = "jsontests/res/files/eth/VMTests/vmIOandFlowOperations"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct VmIOandFlowOperations;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmLogTest"]
+#[directory = "jsontests/res/files/eth/VMTests/vmLogTest"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct Log;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmPushDupSwapTest"]
+#[directory = "jsontests/res/files/eth/VMTests/vmPushDupSwapTest"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct PushDupSwap;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmRandomTest"]
+#[directory = "jsontests/res/files/eth/VMTests/vmRandomTest"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct Random;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmSha3Test"]
+#[directory = "jsontests/res/files/eth/VMTests/vmSha3Test"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct Sha3;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmSystemOperations"]
+#[directory = "jsontests/res/files/eth/VMTests/vmSystemOperations"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct SystemOperations;
 
 #[derive(JsonTests)]
-#[directory = "jsontests/res/files/vmTests"]
+#[directory = "jsontests/res/files/eth/VMTests/vmTests"]
 #[test_with = "jsontests::util::run_test"]
 #[cfg_attr(feature = "bench", bench_with = "jsontests::util::run_bench")]
 struct VM;
