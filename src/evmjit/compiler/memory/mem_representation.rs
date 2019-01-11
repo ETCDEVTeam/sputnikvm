3cfea460c26fec71739e502cc327cf610291190a
Michael Collison <collison956@gmail.com>
2019-01-10 23:08:57 -0700

    Add method to check for MemoryRepresntationType, use new method in units tests and add new constructor for MemoryRepresentationType

--- src/evmjit/compiler/memory/mem_representation.rs
+++ src/evmjit/compiler/memory/mem_representation.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use std::ffi::CString;
 use singletonum::{Singleton, SingletonInit};
 use inkwell::context::Context;
 use inkwell::builder::Builder;
@@ -8,6 +9,7 @@ use inkwell::types::StructType;
 use inkwell::types::PointerType;
 use inkwell::values::PointerValue;
 use inkwell::AddressSpace;
+use evmjit::BasicTypeEnumCompare;
 
 #[derive(Debug, Singleton)]
 
@@ -49,7 +51,48 @@ impl MemoryRepresentationType {
     pub fn get_ptr_type(&self) -> PointerType {
         self.memory_ptr_type
     }
+
+    pub fn is_mem_representation_type(a_struct: &StructType) -> bool {
+        if !a_struct.is_sized() {
+            return false;
+        }
+
+        if a_struct.count_fields() != 3 {
+            return false;
+        }
+
+        if a_struct.is_packed() {
+            return false;
+        }
+
+        if a_struct.is_opaque() {
+            return false;
+        }
+
+        if a_struct.get_name() != Some(&*CString::new("LinearMemory").unwrap()) {
+            return false;
+        }
+
+        let field1 = a_struct.get_field_type_at_index(0).unwrap();
+        if !field1.is_int256() {
+            return false;
+        }
+
+        let field2 = a_struct.get_field_type_at_index(1).unwrap();
+        if !field2.is_int64() {
+            return false;
+        }
+
+        let field3 = a_struct.get_field_type_at_index(2).unwrap();
+        if !field3.is_int64() {
+            return false;
+        }
+
+        true
+
+    }
 }
+
 
 pub struct MemoryRepresentation<'a> {
     m_context: &'a Context,
@@ -59,8 +102,23 @@ pub struct MemoryRepresentation<'a> {
 }
 
 impl<'a> MemoryRepresentation<'a> {
-    pub fn new(name: &str, context: &'a Context,
-               builder: &'a Builder, module: &'a Module) -> MemoryRepresentation<'a> {
+
+    pub fn new_with_mem(allocated_memory: PointerValue, context: &'a Context,
+                        builder: &'a Builder, module: &'a Module) -> MemoryRepresentation<'a> {
+        let mem_type = MemoryRepresentationType::get_instance(context).get_type();
+        builder.build_store(allocated_memory, mem_type.const_zero());
+
+        MemoryRepresentation {
+            m_context: context,
+            m_builder: builder,
+            m_module: module,
+            m_memory: allocated_memory
+        }
+
+    }
+
+    pub fn new_with_name(name: &str, context: &'a Context,
+                         builder: &'a Builder, module: &'a Module) -> MemoryRepresentation<'a> {
         let mem_type = MemoryRepresentationType::get_instance(context).get_type();
         let alloca_result = builder.build_alloca(mem_type, name);
         builder.build_store(alloca_result, mem_type.const_zero());
@@ -77,3 +135,39 @@ impl<'a> MemoryRepresentation<'a> {
         MemoryRepresentationType::get_instance(self.m_context).get_type()
     }
 }
+
+#[test]
+
+fn test_memory_representation_type() {
+    let context = Context::create();
+    let mem_type_singleton = MemoryRepresentationType::get_instance(&context);
+    let mem_struct = mem_type_singleton.get_type();
+
+    assert!(MemoryRepresentationType::is_mem_representation_type (&mem_struct));
+
+    let mem_struct_ptr = mem_type_singleton.get_ptr_type();
+    assert!(mem_struct_ptr.get_element_type().is_struct_type());
+    assert!(MemoryRepresentationType::is_mem_representation_type (mem_struct_ptr.get_element_type().as_struct_type()));
+
+    let evm_word_t = context.custom_width_int_type(256);
+    let size_t = context.i64_type();
+
+    let fields = [evm_word_t.into(),
+                  size_t.into(),
+                  context.i32_type().into()];
+
+    let mem_struct2 = context.opaque_struct_type("LinearMemory");
+    mem_struct2.set_body(&fields, false);
+
+    assert!(!MemoryRepresentationType::is_mem_representation_type (&mem_struct2));
+}
+
+//#[test]
+
+//fn test_memory_representation() {
+//    let context = Context::create();
+//    let module = context.create_module("test_module");
+//    let builder = context.create_builder();
+//
+//    MemoryRepresentation::new(&context, &builder, &module);
+//}
